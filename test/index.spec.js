f258218b8240aa18516b67e077d1eaaea413f50c
Jiri Kaipr <jiri.kaipr@gmail.com>
2019-02-28 11:34:01 +0100

    RF-5179 cleanup & improvements based on review comments

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,16 @@ var expect = require('chai').expect;
 const auth = require('../');
 
 const key = 'key';
+const digests = {
+  NEW: {
+    withOption: '65f2253344287b3c5634a1ce6163fb694b2280b1',
+    withoutOption: 'd38f897889c808c021a8ed97d2caacdac48b8259',
+  },
+  OLD: {
+    withOption: '5957ba2707a51852d32309d16184e8adce9c4d8e',
+    withoutOption: '0a41bdf26fac08a89573a7f5efe0a5145f2730df',
+  }
+};
 
 describe('rainforest-auth', () => {
   let rfAuth;
@@ -18,9 +28,7 @@ describe('rainforest-auth', () => {
     const runId = '123456';
 
     beforeEach(() => {
-      rfAuth = new auth(key);
       url = rfAuth.get_run_callback(runId, 'before_run');
-      console.log(url);
       urlSplit = url.split('/');
     });
 
@@ -38,7 +46,6 @@ describe('rainforest-auth', () => {
 
     it('has the correct digest', () => {
       const digest = urlSplit[9];
-      console.log('digest', digest);
       const verified = rfAuth.verify(digest, 'before_run', { run_id: runId });
       expect(verified).to.be.true;
     });
@@ -46,15 +53,15 @@ describe('rainforest-auth', () => {
 
   describe('sign', () => {
     it('returns the expected signature', () => {
-      expect(rfAuth.sign('test', { option: 1 })).to.be.equal('65f2253344287b3c5634a1ce6163fb694b2280b1');
+      expect(rfAuth.sign('test', { option: 1 })).to.be.equal(digests.NEW.withOption);
     });
 
     it('changes the signature with different data', () => {
-      expect(rfAuth.sign('test', { option: 2 })).to.not.be.equal('65f2253344287b3c5634a1ce6163fb694b2280b1');
+      expect(rfAuth.sign('test', { option: 2 })).to.not.be.equal(digests.NEW.withOption);
     });
 
     it('works with no options parameter', () => {
-      expect(rfAuth.sign('test')).to.equal('d38f897889c808c021a8ed97d2caacdac48b8259');
+      expect(rfAuth.sign('test')).to.equal(digests.NEW.withoutOption);
     });
 
     describe('key hash is null', () => {
@@ -70,15 +77,15 @@ describe('rainforest-auth', () => {
 
   describe('sign_old', () => {
     it('returns the expected signature', () => {
-      expect(rfAuth.sign_old('test', { option: 1 })).to.be.equal('5957ba2707a51852d32309d16184e8adce9c4d8e');
+      expect(rfAuth.sign_old('test', { option: 1 })).to.be.equal(digests.OLD.withOption);
     });
 
     it('changes the signature with different data', () => {
-      expect(rfAuth.sign_old('test', { option: 2 })).to.not.be.equal('5957ba2707a51852d32309d16184e8adce9c4d8e');
+      expect(rfAuth.sign_old('test', { option: 2 })).to.not.be.equal(digests.OLD.withOption);
     });
 
     it('works with no options parameter', () => {
-      expect(rfAuth.sign_old('test')).to.equal('0a41bdf26fac08a89573a7f5efe0a5145f2730df');
+      expect(rfAuth.sign_old('test')).to.equal(digests.OLD.withoutOption);
     });
 
     describe('key hash is null', () => {
@@ -93,30 +100,27 @@ describe('rainforest-auth', () => {
   });
 
   describe('verify', () => {
-    const old_digest = '5957ba2707a51852d32309d16184e8adce9c4d8e';
-    const digest = '65f2253344287b3c5634a1ce6163fb694b2280b1';
-
     it('returns true for a valid signature', () => {
-      expect(rfAuth.verify(digest, 'test', { option: 1 })).to.be.true;
+      expect(rfAuth.verify(digests.NEW.withOption, 'test', { option: 1 })).to.be.true;
     });
 
     it('returns true for a valid old signature', () => {
-      expect(rfAuth.verify(old_digest, 'test', { option: 1 })).to.be.true;
+      expect(rfAuth.verify(digests.OLD.withOption, 'test', { option: 1 })).to.be.true;
     });
 
     it('returns false for a bad signature', () => {
-      expect(rfAuth.verify(digest, 'test', { option: 2 })).to.be.false;
+      expect(rfAuth.verify(digests.NEW.withOption, 'test', { option: 2 })).to.be.false;
     });
 
     it('returns false for a bad old signature', () => {
-      expect(rfAuth.verify(old_digest, 'test', { option: 2 })).to.be.false;
+      expect(rfAuth.verify(digests.OLD.withOption, 'test', { option: 2 })).to.be.false;
     });
 
     it('works with no options parameter', () => {
       // OLD
-      expect(rfAuth.verify('0a41bdf26fac08a89573a7f5efe0a5145f2730df', 'test')).to.be.true;
+      expect(rfAuth.verify(digests.OLD.withoutOption, 'test')).to.be.true;
       // NEW
-      expect(rfAuth.verify('d38f897889c808c021a8ed97d2caacdac48b8259', 'test')).to.be.true;
+      expect(rfAuth.verify(digests.NEW.withoutOption, 'test')).to.be.true;
     });
   });
 });
